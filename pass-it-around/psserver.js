2ab874a3d95105857b38edbd1d64713adcff9f0d
isaacasher97 <isaac.asher97@gmail.com>
2023-06-10 17:23:54 -0400

    pass it around completed

---
+++ pass-it-around/psserver.js
@@ -0,0 +1,48 @@
+const express = require("express")
+
+
+const app = express()
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send(`<h1>99 Bottles of beer on the wall</h1><a href="/98">Take one down, pass it around</a>`);
+  });
+  
+  app.get('/:number_of_bottles', (req, res) => {
+      // Extract the number of bottles from the URL
+    const numberOfBottles = parseInt(req.params.number_of_bottles);
+    let song = ''; // Initialize the variable to store the song lyrics and HTML code
+  
+    if (numberOfBottles === 0) {
+       // If there are no more bottles, display the appropriate message  
+      song += `<h1>0 bottles of beer on the wall.</h1>`;
+    } else {
+      const nextBottles = numberOfBottles - 1;  // Calculate the number of bottles for the next verse
+
+      // Add the current verse to the song lyrics and HTML code
+      song += `<h1>${numberOfBottles} Bottle${numberOfBottles !== 1 ? 's' : ''} of beer on the wall.</h1>`;
+  
+      if (nextBottles > 0) {
+        // If there are more bottles remaining, display the "Take one down, pass it around" link
+        song += `<a href="/${nextBottles}">Take one down, pass it around</a>`;
+      }
+    }
+  
+     // Add the "Start over" link to redirect the user back to the home page
+    song += `<br><br><a href="/">Start over</a>`;
+  
+    res.send(song); // Send the song lyrics and HTML code to the user
+  });
+
+
+
+
+
+
+
+
+//Listener
+app.listen(3500, (req, res) => {
+    console.log("Homework server running on port 3500!")
+})
